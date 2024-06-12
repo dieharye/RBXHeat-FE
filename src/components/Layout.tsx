ce894364e519bc8acf8887ba15f59263766e62ba
dieharye <alexnieto.collin@gmail.com>
2024-06-13 08:12:34 +0900

    feat:hover effect

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import React, { ReactNode } from "react";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import { ModalProvider } from "@/contexts/ModalProvider";
+import SigninModal from "./SignInModal";
 
 interface LayoutProps {
   children?: ReactNode;
@@ -9,11 +13,14 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <main className="bg-primary-100 flex">
-      <Sidebar />
-      <div className="w-[calc(100%-360px)]">
-        <Header />
-        {children}
-      </div>
+      <ModalProvider>
+        <Sidebar />
+        <div className="w-[calc(100%-360px)]">
+          <Header />
+          {children}
+        </div>
+        <SigninModal />
+      </ModalProvider>
     </main>
   );
 }
