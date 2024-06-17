af680f689e092b78315874454b339301f95ad84d
black-rust <michaelgordon.kratos@gmail.com>
2024-06-18 03:07:41 +0900

    responsive is in progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,24 +10,24 @@ export default function Home() {
   return (
     <Layout>
       <div className="p-8">
-        <div className="flex items-center gap-5">
-          <button className="uppercase text-[#fff] font-bold text-sm bg-blue-100 hover:bg-primary-500 duration-200 rounded px-8 py-2 h-[35px]">
+        <div className="flex lg:flex-row flex-col items-center gap-5 justify-between">
+          <button className="uppercase text-[#fff] font-bold text-sm bg-blue-100 hover:bg-primary-500 duration-200 rounded px-8 py-2 h-[35px] w-full lg:w-fit">
             Create
           </button>
-          <button className="uppercase text-[#fff] font-bold text-sm bg-blue-500 gap-2 hover:bg-primary-500 duration-200 rounded px-6 py-2 flex items-center h-[35px] ">
+          <button className="uppercase text-[#fff] font-bold text-sm bg-blue-500 gap-2 hover:bg-primary-500 duration-200 rounded px-6 py-2 flex justify-center items-center h-[35px] w-full lg:w-fit">
             <div className="relative"> 
               <Image src="/icons/history.svg" height={20} width={20} alt=""/>
             </div>
             History
           </button>
-          <div className="h-0.5 bg-gradient-bar w-[calc(100%-246px/2-5px*5-220px)]" />
-          <button className="uppercase w-[246px] text-[#fff] font-bold bg-blue-100 gap-2 hover:bg-primary-500 duration-200 rounded pl-2 pr-24 pt-1 pb-3">
-            <span className="block text-[10px] text-left">Total Value</span>
-            <span className="block mt-2 text-left">18.58M</span>
+          <div className="h-0.5 bg-gradient-bar hidden 2xl:block w-[calc(100%-246px/2-5px*5-700px)]" />
+          <button className="uppercase w-full lg:w-[246px] text-[#fff] font-bold bg-blue-100 gap-2 hover:bg-primary-500 duration-200 rounded px-2 pt-1 pb-3 text-center lg:text-left">
+            <span className="block text-[10px]">Total Value</span>
+            <span className="block mt-2">18.58M</span>
           </button>
-          <button className="uppercase w-[246px] text-[#fff] font-bold bg-blue-100 gap-2 hover:bg-primary-500 duration-200 rounded pl-2 pr-24 pt-1 pb-3">
-            <span className="block text-[10px] text-left">Total Games</span>
-            <span className="block mt-2 text-left">23</span>
+          <button className="uppercase w-full lg:w-[246px] text-[#fff] font-bold bg-blue-100 gap-2 hover:bg-primary-500 duration-200 rounded px-2 pt-1 pb-3 text-center lg:text-left">
+            <span className="block text-[10px]">Total Games</span>
+            <span className="block mt-2">23</span>
           </button>
         </div>
         <Game />
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,6 +5,7 @@ import Header from "./Header";
 import Sidebar from "./Sidebar";
 import { ModalProvider } from "@/contexts/ModalProvider";
 import SigninModal from "./SignInModal";
+import ChatToggleButton from "./ChatToggleButton"
 
 interface LayoutProps {
   children?: ReactNode;
@@ -14,10 +15,11 @@ export default function Layout({ children }: LayoutProps) {
   return (<>
     <ModalProvider>
       <Header />
-      <main className="bg-primary-100 flex">
-        <div className="xl:w-[calc(100%-360px)] w-full">
+      <main className="bg-primary-100 flex relative overflow-hidden">
+        <div className="xl:w-[calc(100%-360px)] w-full duration-200">
           {children}
         </div>
+        <ChatToggleButton />
         <SigninModal />
         <Sidebar />
       </main>
